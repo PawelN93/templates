0ec7c0f8caa357b81d865e007fe99343e6c185a4
PawelN93 <paweln1993@gmail.com>
2023-03-08 18:43:19 +0100

    Added task text

--- 01-basics/my_solutions/zadanie1/makeComplex.cpp
+++ 01-basics/my_solutions/zadanie1/makeComplex.cpp
@@ -1,3 +1,10 @@
+// Zadanie
+// Napisz funkcję makeComplex(), która tworzy liczbę zespoloną std::complex z dwóch dostarczonych liczb. Jeśli typy tych liczb są różne, powinna utworzyć się liczba zespolona z typem pierwszego parametru.
+
+// std::complex<int> a = makeComplex(4, 5);        // both ints
+// std::complex<double> b = makeComplex(3.0, 2.0); // both doubles
+// std::complex<int> c = makeComplex(1, 5.0); // int, double -> takes int
+
 #include <iostream>
 #include <complex>
 #include "makeComplexTemplate.hpp"
